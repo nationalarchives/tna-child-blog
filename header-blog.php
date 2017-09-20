337b91b30db4f2b1acfdc706a98fb2b9124e1a07
Chris Bishop <domingobishop@gmail.com>
2017-09-20 12:45:16 +0100

    RSS feed link

--- header-blog.php
+++ header-blog.php
@@ -10,31 +10,39 @@
 			<div class="col-md-12" role="banner">
 				<article class="banner feature-img feature-img-bg" style="background-image: url(<?php echo esc_attr( get_option('blog_header_img') ); ?>);">
 					<div class="entry-header">
-						<h1>Blog</h1>
+						<h1>
+							<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+								Blog
+							</a>
+						</h1>
 					</div>
 					<?php get_blog_image_caption(
-						get_option('blog_img_caption'),
-						get_option('blog_img_url')
+						esc_attr( get_option('blog_img_caption') ),
+						esc_url( get_option('blog_img_url') )
 					); ?>
 					<div class="entry-content">
 						<div class="col-md-2">
-
+							<div class="follow-us">
+								<a href="http://www.nationalarchives.gov.uk/rss/" title="Follow us via RSS">
+									<img src="https://nationalarchives.gov.uk/wp-content/themes/tna-base/img/social/rss.png" alt="RSS logo"></a>
+								Subscribe
+							</div>
 						</div>
 						<div class="col-md-6">
-							<form action="<?php bloginfo('url'); ?>" method="get">
+							<!-- <form action="<?php bloginfo('url'); ?>" method="get">
 								<select name="archive-dropdown" onchange="document.location.href=this.options[this.selectedIndex].value;">
 									<option value=""><?php echo esc_attr( __( 'Select Month' ) ); ?></option>
 									<?php wp_get_archives( array( 'type' => 'monthly', 'format' => 'option', 'show_post_count' => 1 ) ); ?>
 								</select>
 								<?php wp_dropdown_users(array('name' => 'author','show_option_none' => 'Select author...')); ?>
 								<input class="sr-only" type="submit" name="submit" />
-							</form>
+							</form> -->
 						</div>
 						<div class="col-md-4">
 							<div class="search-wrapper">
 								<form id="search-form" method="get" role="search" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 									<label class="sr-only" for="s">Search</label>
-									<input type="text" name="s" id="s" class="search-field" placeholder="Search our Blog" />
+									<input type="text" name="s" id="s" class="search-field" placeholder="Search our blog" />
 									<input type="submit" id="searchsubmit" class="search-button search-button-blog" alt="Submit search" value="" title="Submit" />
 								</form>
 							</div>
