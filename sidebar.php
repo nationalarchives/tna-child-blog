4a683017dd2673e915c1bfb60b8ad2b15bf45245
Chris Bishop <domingobishop@gmail.com>
2017-09-20 13:45:13 +0100

    added sidebar

---
+++ sidebar.php
@@ -0,0 +1,26 @@
+<aside id="sidebar" class="col-xs-12 col-sm-4" role="complementary">
+	<div class="sidebar-content">
+		<div class="sidebar-header">
+			<h2>
+				Sidebar
+			</h2>
+		</div>
+		<div class="sidebar-nav clearfix">
+			<ul class="sibling">
+				<li><a>item</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="sidebar-content">
+		<div class="sidebar-header">
+			<h2>
+				Sidebar
+			</h2>
+		</div>
+		<div class="sidebar-nav clearfix">
+			<ul class="sibling">
+				<li><a>item</a></li>
+			</ul>
+		</div>
+	</div>
+</aside>
