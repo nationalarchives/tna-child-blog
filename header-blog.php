9f62715dbbc12ee373e2a5d98eb6105705dc5258
Chris Bishop <domingobishop@gmail.com>
2017-09-20 12:02:37 +0100

    search form

--- header-blog.php
+++ header-blog.php
@@ -17,7 +17,28 @@
 						get_option('blog_img_url')
 					); ?>
 					<div class="entry-content">
+						<div class="col-md-2">
 
+						</div>
+						<div class="col-md-6">
+							<form action="<?php bloginfo('url'); ?>" method="get">
+								<select name="archive-dropdown" onchange="document.location.href=this.options[this.selectedIndex].value;">
+									<option value=""><?php echo esc_attr( __( 'Select Month' ) ); ?></option>
+									<?php wp_get_archives( array( 'type' => 'monthly', 'format' => 'option', 'show_post_count' => 1 ) ); ?>
+								</select>
+								<?php wp_dropdown_users(array('name' => 'author','show_option_none' => 'Select author...')); ?>
+								<input class="sr-only" type="submit" name="submit" />
+							</form>
+						</div>
+						<div class="col-md-4">
+							<div class="search-wrapper">
+								<form id="search-form" method="get" role="search" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+									<label class="sr-only" for="s">Search</label>
+									<input type="text" name="s" id="s" class="search-field" placeholder="Search our Blog" />
+									<input type="submit" id="searchsubmit" class="search-button search-button-blog" alt="Submit search" value="" title="Submit" />
+								</form>
+							</div>
+						</div>
 					</div>
 				</article>
 			</div>
