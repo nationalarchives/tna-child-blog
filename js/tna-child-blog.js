924fa14771a9c026edf1a5de19e3d00a8649c011
Chris Bishop <domingobishop@gmail.com>
2017-09-25 14:26:03 +0100

    js submit form on change

--- js/tna-child-blog.js
+++ js/tna-child-blog.js
@@ -2,12 +2,6 @@
  * Blog
  */
 
-$(document).ready(function () {
-    var dropdown = document.getElementById("author");
-    function onAuChange() {
-        if ( dropdown.options[dropdown.selectedIndex].value > 0 ) {
-            location.href = "?author="+dropdown.options[dropdown.selectedIndex].value;
-        }
-    }
-    dropdown.onchange = onAuChange;
-});
+$('.author-list').on('change', function () {
+    $(this).submit();
+})
