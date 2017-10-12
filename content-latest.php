735c8d033a41d9728c4febd240fdbc7b7be4c0e1
Chris Bishop <domingobishop@gmail.com>
2017-10-12 11:50:03 +0100

    view all posts button

--- content-latest.php
+++ content-latest.php
@@ -41,8 +41,16 @@ $the_query = new WP_Query( $args ); ?>
 	<?php endwhile; ?>
 	<!-- end of the loop -->
 
-	</div>
 	<?php wp_reset_postdata(); ?>
+
+	<?php if ( get_option('blog_all_posts_url') ) { ?>
+		<div class="col-md-12">
+			<a href="<?php echo esc_attr( get_option('blog_all_posts_url') ); ?>" class="button pull-right">View all posts</a>
+		</div>
+	<?php } ?>
+
+	</div>
+
 
 <?php else : ?>
 	<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
