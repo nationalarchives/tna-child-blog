19595b6aac7f3f3fbffc2b41ee30c581367f0819
Chris Bishop <domingobishop@gmail.com>
2017-11-02 12:12:50 +0000

    responsive header

--- js/tna-child-blog.js
+++ js/tna-child-blog.js
@@ -5,3 +5,8 @@
 $('.author-list').on('change', function () {
     $(this).submit();
 })
+
+$('.month-list').on('change', function () {
+    var month = document.getElementById("month");
+    window.location = month.value;
+})
