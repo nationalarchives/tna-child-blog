956702bc4d6aa5281d255c84dffd5d5d4022f9f2
Chris Bishop <domingobishop@gmail.com>
2018-04-11 10:20:39 +0100

    blog_type condition for breadcrumbs

--- breadcrumb.php
+++ breadcrumb.php
@@ -1,3 +1,8 @@
+<?php if ( get_option('blog_type') == 'amp' ) {
+	$type = 'Archives Media Player';
+} else {
+	$type = 'Blog';
+} ?>
 <div id="breadcrumb-holder" class="tna-breadcrumb">
 	<div class="container">
 		<div class="row">
@@ -10,9 +15,9 @@
 					</span>
 					<span class="sep">&gt;</span>
 					<?php if ( is_front_page() ) { ?>
-					<span>Blog</span>
+					<span><?php echo $type ?></span>
 					<?php } else { ?>
-					<span><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Blog</a></span>
+					<span><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo $type ?></a></span>
 					<?php } ?>
 					<?php if ( !is_front_page() ) { ?>
 					<span class="sep">&gt;</span>
