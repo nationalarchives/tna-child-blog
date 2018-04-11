62cb7759b9b9c5b0a26641b1918c718742b40b00
Chris Bishop <domingobishop@gmail.com>
2018-04-11 10:17:21 +0100

    video toolbar

--- partials/video.php
+++ partials/video.php
@@ -2,4 +2,7 @@
 	<div class="video-container">
 		<iframe width="560" height="315" src="https://www.youtube.com/embed/6cBLIGvd24w" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 	</div>
+	<div class="video-toolbar text-right">
+		<a href="#">Download media file</a>
+	</div>
 </div>
