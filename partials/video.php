eea940e3330eb8c9f41d08357641e76005e1563e
Chris Bishop <domingobishop@gmail.com>
2018-04-11 11:30:00 +0100

    social buttons on toolbar

--- partials/video.php
+++ partials/video.php
@@ -2,7 +2,14 @@
 	<div class="video-container">
 		<iframe width="560" height="315" src="https://www.youtube.com/embed/6cBLIGvd24w" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 	</div>
-	<div class="video-toolbar text-right">
-		<a href="#">Download media file</a>
+	<div class="video-toolbar clearfix">
+		<div class="col-xs-6">
+			<span class="addthis_toolbox addthis_default_style addthis_20x20_style">
+				<a class="addthis_button_facebook"></a><a class="addthis_button_twitter"></a><a class="addthis_button_compact"></a>
+			</span>
+		</div>
+		<div class="col-xs-6 text-right">
+			<a href="#">Download media file</a>
+		</div>
 	</div>
 </div>
