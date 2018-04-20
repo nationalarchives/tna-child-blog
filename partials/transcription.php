7c532fba63dde2564469adcc0e56ea8a8db011aa
Chris Bishop <domingobishop@gmail.com>
2018-04-20 11:48:53 +0100

    function_exists credits

--- partials/transcription.php
+++ partials/transcription.php
@@ -2,7 +2,7 @@
 	<?php if (get_field('transcription')) { ?>
 	<div id="transcription" class="entry-content entry-media-transcription clearfix">
 		<h2>Transcription</h2>
-		<?php the_field ('transcription'); ?>
+		<?php the_field('transcription'); ?>
 	</div>
 	<?php } ?>
 <?php } ?>
