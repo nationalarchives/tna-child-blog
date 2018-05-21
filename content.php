3f952e7636e0df81678f8c7490432dc972ced36e
Chris Bishop <domingobishop@gmail.com>
2018-05-21 14:53:52 +0100

    addthis js over https

--- content.php
+++ content.php
@@ -36,7 +36,7 @@
 			<div class="addthis_toolbox addthis_default_style addthis_32x32_style">
 				<a class="addthis_button_facebook"></a><a class="addthis_button_twitter"></a><a class="addthis_button_compact"></a><a class="addthis_counter addthis_bubble_style"></a>
 			</div>
-			<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=xa-500565897ff2ca83"></script>
+			<script type="text/javascript" src="https://s7.addthis.com/js/250/addthis_widget.js#pubid=xa-500565897ff2ca83"></script>
 		</div>
 		<?php comments_template(); ?>
 	</article>
