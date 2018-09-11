04329a22ccc9e33697e6d85f1e5b73605e872a30
Chris Bishop <domingobishop@gmail.com>
2018-09-11 18:41:48 +0100

    remove unused vars

--- inc/functions-recommended.php
+++ inc/functions-recommended.php
@@ -59,44 +59,33 @@ function blog_get_meta_og_data( $url ) {
 
 function display_relative_content( $content, $categories='', $tags='' ) {
 
-	//return array
-	$ret = '';
 	$content_links = array();
-
-	/*** a new dom object ***/
 	$dom = new domDocument;
-
-	/*** get the HTML (suppress errors) ***/
 	@$dom->loadHTML($content);
-
-	/*** remove silly white space ***/
 	$dom->preserveWhiteSpace = false;
-
-	/*** get the links from the HTML ***/
 	$links = $dom->getElementsByTagName('a');
-
-	/*** loop over the links ***/
 	foreach ($links as $link_obj)
 	{
 		$link = $link_obj->getAttribute('href');
-		$ret .= $link . ' <br>';
-		if (
-			strpos( $link, 'research-guides' ) !== false ||
-			strpos( $link, 'media.nationalarchives' ) !== false ||
-			strpos( $link, 'blog.nationalarchives' ) !== false
-		) {
+		if ( strpos( $link, '.jpg' ) == false ) {
 			if (
-				(strpos( $link, 'research-guides' ) !== false && !preg_grep( '/research-guides/', $content_links)) ||
-				(strpos( $link, 'media.nationalarchives' ) !== false && !preg_grep( '/media.nationalarchives/', $content_links)) ||
-				(strpos( $link, 'blog.nationalarchives' ) !== false && !preg_grep( '/blog.nationalarchives/', $content_links))
+				strpos( $link, 'research-guides' ) !== false ||
+				strpos( $link, 'media.nationalarchives' ) !== false ||
+				strpos( $link, 'blog.nationalarchives' ) !== false
 			) {
-				array_push( $content_links, $link );
+				if (
+					(strpos( $link, 'research-guides' ) !== false && !preg_grep( '/research-guides/', $content_links)) ||
+					(strpos( $link, 'media.nationalarchives' ) !== false && !preg_grep( '/media.nationalarchives/', $content_links)) ||
+					(strpos( $link, 'blog.nationalarchives' ) !== false && !preg_grep( '/blog.nationalarchives/', $content_links))
+				) {
+					array_push( $content_links, $link );
+				}
 			}
 		}
 	}
 
-	$recommended = '';
 	global $post;
+	$recommended = '';
 	$transient_recommended = get_transient( 'recommended-'.$post->ID );
 
 	if ( !$transient_recommended ) {
