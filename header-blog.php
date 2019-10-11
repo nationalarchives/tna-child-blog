f5139a66e7141276dc74cd0e2aa681a61ac2702c
domingobishop <domingobishop@gmail.com>
2019-10-11 14:25:23 +0100

    header image

--- header-blog.php
+++ header-blog.php
@@ -4,12 +4,12 @@
  */
 ?>
 
-<div class="blog-header">
+<div class="blog-header feature-img">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
                 <?php get_template_part('breadcrumb'); ?>
-                <div class="entry-header">
+                <div class="entry-header" style="background-image: url(<?php echo esc_attr( https_this( get_option('blog_header_img') ) ); ?>);">
                     <div class="h1">
                         <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
                             <?php if ( is_amp() ) {
@@ -19,6 +19,10 @@
                             } ?>
                         </a>
                     </div>
+                    <?php get_blog_image_caption(
+                        esc_attr( get_option('blog_img_caption') ),
+                        esc_url( get_option('blog_img_url') )
+                    ); ?>
                 </div>
             </div>
         </div>
