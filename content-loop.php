99d7db20b0dcf46e2c1b219b11e4643677b0131b
domingobishop <domingobishop@gmail.com>
2019-10-16 15:48:50 +0100

    undefined var error on search results

--- content-loop.php
+++ content-loop.php
@@ -7,17 +7,21 @@
                 the_post();
 
                 $cats = get_the_category();
-                $cat_list = wp_list_pluck( $cats, 'name' );
+                $label = 'Blog';
+                if ( $cats ) {
+                    $cat_list = wp_list_pluck( $cats, 'name' );
 
-                if (in_array('Audio', $cat_list)) {
-                    $label   = 'Audio';
-                } elseif (in_array('Video', $cat_list)) {
-                    $label   = 'Video';
-                } elseif (is_amp()) {
-                    $label   = $cat_list[0];
-                } else {
-                    $label   = $cat_list[0];
+                    if (in_array('Audio', $cat_list)) {
+                        $label   = 'Audio';
+                    } elseif (in_array('Video', $cat_list)) {
+                        $label   = 'Video';
+                    } elseif (is_amp()) {
+                        $label   = $cat_list[0];
+                    } else {
+                        $label   = $cat_list[0];
+                    }
                 }
+
 
                 $image = get_feature_image_url( get_the_ID(), 'medium' );
                 $title   = get_the_title();
