80b3635d7782d6799b2f357d2ef1142eaca5a642
domingobishop <domingobishop@gmail.com>
2019-10-17 17:21:09 +0100

    refactor display cards to accommodate post published date

--- content-item.php
+++ content-item.php
@@ -2,7 +2,7 @@
 
 $cats = get_the_category();
 $cat_list = wp_list_pluck( $cats, 'name' );
-
+$label = 'Blog';
 if (in_array('Audio', $cat_list)) {
     $label   = 'Audio';
 } elseif (in_array('Video', $cat_list)) {
@@ -13,12 +13,16 @@ if (in_array('Audio', $cat_list)) {
     $label   = $cat_list[0];
 }
 
-$image = get_feature_image_url( get_the_ID(), 'large' );
-$title   = get_the_title();
-$url     = get_permalink();
-$excerpt = trim(substr(get_the_excerpt(), 0,160)).'...';
-$date    = '';
-$id      = get_the_ID();
+$args = array(
+    'id'            => get_the_ID(),
+    'url'           => get_permalink(),
+    'title'         => get_the_title(),
+    'description'   => trim(substr(get_the_excerpt(), 0,160)).'...',
+    'image'         => get_feature_image_url( get_the_ID(), 'large' ),
+    'author'        => '',
+    'pub_date'      => get_the_date( 'l j F Y' ),
+    'label'         => $label
+);
 
-echo display_card( $id, $url, $title, $excerpt, $image, $date, $label );
+echo display_card( $args );
 
