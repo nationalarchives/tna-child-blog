774a4759f68ea4fa0d32044cc9d3e7c2b2f987bb
Mihai Diaconita <michaiel.webdesigner@gmail.com>
2021-04-29 15:35:11 +0100

    Remove AddThis widget from Blog

--- content.php
+++ content.php
@@ -32,12 +32,6 @@
 			<p><?php echo $tags_list; ?></p>
 		</div>
 		<?php } ?>
-		<div id="blogsocial">
-			<div class="addthis_toolbox addthis_default_style addthis_32x32_style">
-				<a class="addthis_button_facebook"></a><a class="addthis_button_twitter"></a><a class="addthis_button_compact"></a><a class="addthis_counter addthis_bubble_style"></a>
-			</div>
-			<script type="text/javascript" src="https://s7.addthis.com/js/250/addthis_widget.js#pubid=xa-500565897ff2ca83"></script>
-		</div>
 		<?php comments_template(); ?>
 	</article>
 <?php } ?>
