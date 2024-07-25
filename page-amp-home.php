17583cb86f30b4318b6c81eddf9cb6e55110982b
Andrew Hosgood <mail@andrewhosgood.com>
2024-07-25 17:20:12 +0100

    Accessibility fixes (#80)
    * Remove category tabs
    
    * Add missing echo
    
    * Add extra space between rows of post categories on homepage
    
    * Fix heading levels
    
    * Remove AMP category tabs

--- page-amp-home.php
+++ page-amp-home.php
@@ -23,9 +23,6 @@ get_header(); ?>
 				</div>
 				<div class="col-md-12">
 					<article>
-						<div class="entry-header">
-							<h2 class="separator-heading">Posts by category</h2>
-						</div>
 						<div class="entry-content clearfix">
 							<?php get_template_part('content', 'amp-categories'); ?>
 						</div>
